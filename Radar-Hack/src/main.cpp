d1785b660626acdf25f8272279e12893259b7e0e
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2023-08-15 12:57:25 +0400

    Add project files.

---
+++ Radar-Hack/src/main.cpp
@@ -0,0 +1,8 @@
+#include "memory.h"
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
---
+++ Radar-Hack/src/memory.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <cstdint>
+
+class Memory
+{
+private:
+	DWORD id {};
+	HANDLE hProc {};
+
+public:
+	Memory(const char* name) noexcept;
+
+	DWORD getProcessId()
+	{
+		return this->id;
+	}
+
+	HANDLE getProcessHandle()
+	{
+		return this->hProc;
+	}
+
+	uintptr_t getModuleAddress(const char* name);
+
+	template <typename T>
+	T Read(uintptr_t address)
+	{
+		T value {};
+		ReadProcessMemory(hProc, (LPVOID)address, &value, sizeof(T), 0);
+		return value;
+	}
+
+	template <typename T>
+	bool Write(uintptr_t address, T value)
+	{
+		return WriteProcessMemory(hProc, (LPVOID)address, &value, sizeof(T), 0);
+	}
+
+	~Memory()
+	{
+		if (hProc)
+			CloseHandle(hProc);
+	}
+
+};
